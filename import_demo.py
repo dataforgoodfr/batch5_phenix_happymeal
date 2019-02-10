8bf467484cec7e54fbac7740aa202ca3b2c2c878
F-G Fernandez <fgfm03@hotmail.fr>
2019-02-10 23:24:14 +0100

    Fixed tier completion
    Reuslts were not correct as prices were indexed on kilogram while batch quantities were in grams, added level 1 category pricing to match DB. Fixes #11

--- import_demo.py
+++ import_demo.py
@@ -10,25 +10,29 @@ __version__ = '0.1'
 __maintainer__ = 'Julie Seguela'
 __status__ = 'Development'
 
-# import argparse
+import os
+import argparse
+import numpy as np
 import pandas as pd
+
 from io_mgt import listing_import as li
 from preprocessing import categorization as ct
 from utils.off_mgt import get_product_information, convert_to_gram, parse_qty_info
+from meal_balancer.grouping import create_batches
+from meal_balancer.balance_adjuster import missing_balance, tier_completion
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("commande_id", type=str, help="Enter the commande_id you want to process")
-#args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("order_id", type=int, help="Enter the order_id you want to process")  # 94349, 81708
+parser.add_argument("--batch_qty", type=int, default=500, help="Enter the desired base basket size in grams")
+args = parser.parse_args()
 
 
-listing_file = 'commande_94349.csv'  #  'commande_81708.csv'
 mapping_file = 'data/mapping_off_ideal.csv'
 model_classifier_file = 'data/clf_nutrients_rf_groupeAlim_2_light.sav'
 model_matching_file = 'data/clf_names_nb_light.sav'
 
 
 def parse_phenix_grams(product_name):
-
     phenix_qty = None
     # If several results, take the last one as weight information is usually at the end
     parsing_results = parse_qty_info(product_name)
@@ -43,7 +47,6 @@ def parse_phenix_grams(product_name):
 
 
 def get_off_grams(ean):
-
     off_qty = None
     if pd.notnull(ean):
         # If several results, take the last one as weight information is usually at the end
@@ -58,16 +61,14 @@ def get_off_grams(ean):
 
 def main():
     # Read food listing file
-    input_listing = li.importListing(listing_file)
+    input_listing = li.importListing(f"commande_{args.order_id}.csv")
 
     # Extract quantity in grams
-    # import pdb; pdb.set_trace()
     input_listing.drop(columns='#', inplace=True)
     input_listing['phenix_grams'] = input_listing.Produit_Nom.apply(lambda s, f=parse_phenix_grams: f(s))
     input_listing['off_grams'] = input_listing.EAN.apply(lambda s, f=get_off_grams: f(s))
     input_listing['weight_grams'] = input_listing.phenix_grams
 
-    print("weight added")
 
     # Add food category to the data frame
     input_listing = ct.get_foodGroupFromToDF(input_listing,
@@ -77,22 +78,59 @@ def main():
                                              model_classifier_file=model_classifier_file,
                                              model_matching_file=model_matching_file,
                                              group_name='labelAlim_1')
-    print("category added")
-    print(input_listing)
-    print(input_listing.columns)
 
-    input_listing.columns = ['product_name', 'ean', 'unit_price', 'quantity', 'total_price', 'phenix_grams', 'off_grams', 'weight_grams', 'group_name', 'group_method']
+    input_listing.columns = ['product_name', 'ean', 'unit_price', 'quantity', 'total_price', 'phenix_grams',
+                             'off_grams', 'weight_grams', 'group_name', 'group_method']
     input_listing = input_listing[pd.notnull(input_listing.product_name)]
-    int_cols = ['ean', 'group_method']
-    input_listing[int_cols] = input_listing[int_cols].astype(int)
-    float_cols = ['unit_price', 'quantity', 'total_price']
+    float_cols = ['unit_price', 'total_price', 'quantity']
     input_listing[float_cols] = input_listing[float_cols].astype(float)
+    input_listing.quantity = np.floor(input_listing.quantity)
+    int_cols = ['ean', 'group_method', 'quantity']
+    input_listing[int_cols] = input_listing[int_cols].astype(int)
+
+    # Get ideal split
+    ideal_split = pd.read_csv(os.path.join('data', 'food_categories.csv'), sep=';')
+    ideal_split = ideal_split.drop_duplicates(['labelAlim_1', 'idealDistrib_1'])
+    # cat_distrib = dict(zip(ideal_split['codeAlim_1'].values, ideal_split['idealDistrib_1'].values))
+    ideal_split = pd.Series(ideal_split.idealDistrib_1.values, index=ideal_split.labelAlim_1).to_dict()
+
+    # Get algo result
+    selection = input_listing[pd.notnull(input_listing.weight_grams)].filter(
+        items=['quantity', 'weight_grams', 'group_name'])
+    items = []
+    for idx, row in selection.iterrows():
+        # Quantity
+        items.extend([dict(category=row['group_name'], quantity=row['weight_grams'])
+                      for ex in range(row['quantity'])])
+
+    all_batches, storage, big_items, unidentified, tmp_items, loss = create_batches(items, ideal_split,
+                                                                                    batch_qty=args.batch_qty,
+                                                                                    overflow_thresh=0.5,
+                                                                                    underflow_thresh=0.2)
+    # Ready now to apply meal balancer
+    req_portions = missing_balance(storage, ideal_split)
+    # Prices for groupeAlim_2
+    bare_prices = pd.read_csv(os.path.join('data', 'categ_prices.csv'))
+    # Mapping
+    mapping_groups = pd.read_csv(mapping_file, sep=';', encoding='UTF-8')
+    categ_prices = pd.Series(bare_prices.loc[bare_prices.aggregation_level == 1, 'average_price'].values,
+                             index=bare_prices.loc[bare_prices.aggregation_level == 1, 'category']).to_dict()
+
+    # categ_prices = {cat: 1e-3 for cat in req_portions.keys()}
+    res = tier_completion(req_portions, categ_prices, ideal_split, batch_qty=args.batch_qty)
+
+
+    # Result printing
+    print(f"Listing split: {input_listing.groupby('group_name').quantity.sum()}")
+    print(f"Missing categories: {[elt for elt, val in ideal_split.items() if val > 0 and elt not in input_listing.group_name]}")
+    print(f'Big items: {len(big_items)}')
+    print(f'Unidentified: {len(unidentified)}')
+    for tier in res:
+        print(f"{tier.get('nb_meals')} meals at {tier.get('meal_price')} unit price (buying {tier.get('categ_bought')})")
 
     # write output file
-    input_listing.to_csv("data/output/" + listing_file,
+    input_listing.to_csv(os.path.join('data', 'output', f"commande_{args.order_id}.csv"),
                          sep=';', encoding='UTF-8', index=False)
-
-    # Ready now to apply meal balancer
 
 
 if __name__ == '__main__':
--- meal_balancer/balance_adjuster.py
+++ meal_balancer/balance_adjuster.py
@@ -25,22 +25,24 @@ def missing_balance(unbalanced_items, split_categ):
     """
 
     # Determine the category availability
-    available_portions = {item_categ: unbalanced_items.get(item_categ, Pile()).size() for item_categ in split_categ}
+    available_portions = {item_categ: unbalanced_items.get(item_categ, Pile()).size()
+                          for item_categ, val in split_categ.items() if val > 0}
     max_portions = max(available_portions.values())
 
     # Determine what we still need
-    required_portions = {item_categ: max_portions - categ_count for item_categ, categ_count in available_portions.items()}
+    required_portions = {item_categ: max_portions - categ_count
+                         for item_categ, categ_count in available_portions.items()}
 
     return required_portions
 
 
-def tier_completion(required_portions, categ_prices, batch_qty=500):
+def tier_completion(required_portions, categ_prices, ideal_split, batch_qty=500):
     """
     Complete meals by spending-tier
     Args:
-        unbalanced_items (dict): dictionary item_categ --> list of balanced portions
+        required_portions (dict): dictionary item_categ --> list of balanced portions
         categ_prices (dict): dictionary item_categ --> average price per kilogram
-        btach_qty (float)
+        batch_qty (float): size of the batch (grams)
     Returns:
         required_items (dict): dictionary item categ --> missing quantity
     """
@@ -50,20 +52,22 @@ def tier_completion(required_portions, categ_prices, batch_qty=500):
     # Categories to buy
     tmp_dict = {key: val for key, val in required_portions.items()}
 
+    # Sort by the number of portions required
     tmp_vals = sorted(list(set(tmp_dict.values())), reverse=True)
+    # Get a list of each categ missing by tier
     buying_categs = [[]] * (len(tmp_vals) - 1)
     for categ, categ_need in tmp_dict.items():
         if categ_need > 0:
-            try:
-                buying_categs[tmp_vals.index(categ_need)].append(categ)
-            except:
-                import pdb; pdb.set_trace()
+            buying_categs[tmp_vals.index(categ_need)].append(categ)
     # Loop on increasingly needed categ
     running_categs = []
     for idx, categs in enumerate(buying_categs):
         running_categs.extend(categs)
         # get the price per extra meal
-        meal_prices.append(batch_qty * sum([categ_prices[cat] for cat in running_categs]))
+        meal_prices.append(batch_qty / 1000 * sum([ideal_split[cat] * categ_prices[cat] for cat in running_categs]))
         nb_meals.append(tmp_vals[idx] - tmp_vals[idx + 1])
 
-    return meal_prices, nb_meals
+    res = [dict(nb_meals=tier_meals, meal_price=meal_prices[idx], categ_bought=buying_categs[idx])
+           for idx, tier_meals in enumerate(nb_meals)]
+
+    return res
